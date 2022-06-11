efd623bdd70b44fa78493715856b0db2ce3ea792
brokerr1 <solovievdima1@gmail.com>
2022-06-11 15:42:29 +0300

    2

--- 2/Program.cs
+++ 2/Program.cs
@@ -2,3 +2,13 @@
 // натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+Console.Write("Введите первое число: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ResultN(numberM, numberN));
+int ResultN(int m, int n)
+{
+    if (m > n) return 0;
+    else return m + ResultN(m + 1, n);
+}

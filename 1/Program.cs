0d33468ac8111aa822fa08fd33ea79cce787ff27
brokerr1 <solovievdima1@gmail.com>
2022-06-11 15:23:49 +0300

    1

--- 1/Program.cs
+++ 1/Program.cs
@@ -2,3 +2,15 @@
 // натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ResultN(number);
+void ResultN(int n)
+{
+    if (n == 0) return;
+    else
+    {
+        Console.Write($"{n} ");
+        ResultN(n - 1);
+    }
+}
